64c1df88f1be6cf83d28c1152ffee5464300d5a3
aayushkc2158 <aayushkc2158@gmail.com>
2023-11-10 23:04:44 +0545

    feat: second commit

--- src/app/home/stable-signals/stable-signals.component.ts
+++ src/app/home/stable-signals/stable-signals.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
 
+function input() {
+  return 2;
+}
+
 @Component({
   selector: 'app-stable-signals',
-  standalone: true,
+  standalone:true,
   imports: [CommonModule,RouterModule],
   templateUrl: './stable-signals.component.html',
   styleUrl: './stable-signals.component.scss'
 })
 export class StableSignalsComponent {
+
+  // let computer = signal(1);
+  //
+  // const result = computed(() => input() + 5); // result would now yield 6
+  //
+  // computer.set(2);
+  //
 
 }
